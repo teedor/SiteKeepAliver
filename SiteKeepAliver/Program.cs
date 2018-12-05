aa1fdc7bcde1e18ca7a5196a905ef1956e83c1ef
Stephen Anderson <stephen.anderson@madetoengage.com>
2018-12-05 11:58:35 +0000

    initial app created

--- SiteKeepAliver/Program.cs
+++ SiteKeepAliver/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace SiteKeepAliver
 {
@@ -10,6 +12,14 @@ namespace SiteKeepAliver
     {
         static void Main(string[] args)
         {
+            var lines = File.ReadLines("urls.txt");
+            foreach (var line in lines)
+            {
+                var client = new RestClient(line);
+                var request = new RestRequest();
+                request.Method = Method.GET;
+                var response = client.Execute(request);
+            }
         }
     }
 }
